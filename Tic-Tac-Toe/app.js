c8ea3ea0f763056dfab918e5ef10f02cac9401cb
daventsai <daventsai75@gmail.com>
2023-03-08 12:05:30 -0600

    evaluating win condition completed

--- Tic-Tac-Toe/app.js
+++ Tic-Tac-Toe/app.js
@@ -53,16 +53,89 @@ function fillInSquare(event){
         turn++;
         let arraySplit = tempSquarePosition.split("");
         gameState.board[parseInt(arraySplit[0])][parseInt(arraySplit[1])] = playerAssignment;
-        console.log(gameState.board);
-        //changes to next player
-        if (playerAssignment === 'X'){
-          playerAssignment = 'O';
-        }
-        else if (playerAssignment === 'O'){
-          playerAssignment = 'X';
-        }
+        evalWinCondition();
+        changePlayers();
     }
     else{
         window.alert("The square is already filled in!");
     }
+}
+
+function changePlayers(){
+  if (playerAssignment === 'X'){
+    playerAssignment = 'O';
+  }
+  else if (playerAssignment === 'O'){
+    playerAssignment = 'X';
+  }
+}
+
+function evalWinCondition(){
+//Row Win
+  let gameWon = false;
+  for (let i=0;i<gameState.board.length;i++){
+    let rowCalc = 0;
+    for (let j=0;j<gameState.board.length;j++){
+      if (gameState.board[i][j]==='X'){
+        rowCalc++;
+      }
+      else if (gameState.board[i][j]==='O'){
+        rowCalc--;
+      }
+      if (rowCalc === gameState.board.length && gameWon === false){
+        alert("Player X Wins!");
+        gameWon = true;
+      }
+      else if (rowCalc === -gameState.board.length && gameWon === false){
+        alert("Player O Wins!");
+        gameWon = true;
+      }
+    }
+  }
+  //Column Win
+  for (let i=0;i<gameState.board.length;i++){
+    let colCalc = 0;
+    for (let j=0;j<gameState.board.length;j++){
+      if (gameState.board[j][i]==='X'){
+        colCalc++;
+      }
+      else if (gameState.board[j][i]==='O'){
+        colCalc--;
+      }
+      if (colCalc === gameState.board.length && gameWon === false){
+        alert("Player X Wins!");
+        gameWon = true;
+      }
+      else if (colCalc === -gameState.board.length && gameWon === false){
+        alert("Player O Wins!");
+        gameWon = true;
+      }
+    }
+  }
+  //Diagonal Win
+  if ((gameState.board[0][0] + gameState.board[1][1] + gameState.board[2][2] === 'XXX' ||
+      gameState.board[0][2] + gameState.board[1][1] + gameState.board[2][0] === 'XXX') && gameWon === false){
+    alert("Player X Wins!");
+    gameWon = true;
+  }
+  else if ((gameState.board[0][0] + gameState.board[1][1] + gameState.board[2][2] === 'OOO' ||
+          gameState.board[0][2] + gameState.board[1][1] + gameState.board[2][0] === 'OOO') && gameWon === false){
+    alert("Player O Wins!");
+    gameWon = true;
+  }
+  //Check ties
+  if (gameWon === false){
+    let emptySquares = 9;
+    for (let i=0;i<gameState.board.length; i++){
+      for (let j=0;j<gameState.board.length; j++){
+        if (gameState.board[i][j]!==null){
+          emptySquares--;
+        }
+      }
+    }
+    if (emptySquares ===0){
+      alert("There is a tie");
+      gameWon = true;
+    }
+  }
 }
